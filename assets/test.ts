18efd16258972e5722e427762c84947eb94c4c5a
Manuel Roldán <ercaximbero96@gmail.com>
2022-06-23 00:12:04 +0200

    Auto-generated commit [ci skip]

--- assets/test.ts
+++ assets/test.ts
@@ -1,257 +1,277 @@
 export const TEST = [
     [
         [
-            71,
-            71,
-            61
+            50,
+            46,
+            48
         ],
         {
-            "id": "6917529734471420806",
+            "id": "6917529756259041037",
             "hash": 3787517196,
             "name": "Brazaletes Solares",
             "iconURL": "https://www.bungie.net/common/destiny2_content/icons/17c01ac034a7194e2de22d1ac487bb70.jpg",
-            "mobility": 2,
-            "resilience": 13,
-            "recovery": 18,
-            "discipline": 10,
-            "intellect": 7,
-            "strength": 16,
+            "mobility": 18,
+            "resilience": 8,
+            "recovery": 6,
+            "discipline": 19,
+            "intellect": 9,
+            "strength": 2,
             "armorType": "GAUNTLETS",
             "classType": 2,
             "isExotic": true
         },
         [
-            "RESISTENCIA",
-            "RESISTENCIA",
             "DISCIPLINA",
-            "RESISTENCIA",
-            "RECUPERACION"
+            "DISCIPLINA",
+            "DISCIPLINA",
+            "DISCIPLINA",
+            "INTELECTO"
         ],
-        [],
+        [
+            "CONVERSOR DE ENERGIA",
+            "RESERVAS EXTRA",
+            "AMISTADES PODEROSAS",
+            "CARGA CON PRECISION"
+        ],
         [
             10,
-            10,
-            10
+            0,
+            -10
         ],
         [
             {
-                "id": "6917529549279540640",
-                "hash": 2345799798,
-                "name": "Grebas Sueño de la perdición",
-                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/2627c43a8d22136c3ad1f061bf92bc8e.jpg",
+                "id": "6917529705088755611",
+                "hash": 3465627817,
+                "name": "Túnica Lealtad Acolmillada",
+                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/5fe1451f524165401615226ab2bdc50c.jpg",
                 "mobility": 2,
-                "resilience": 8,
-                "recovery": 23,
-                "discipline": 21,
-                "intellect": 2,
-                "strength": 10,
-                "armorType": "LEGS",
-                "classType": 0,
+                "resilience": 2,
+                "recovery": 30,
+                "discipline": 9,
+                "intellect": 21,
+                "strength": 2,
+                "armorType": "CHEST",
+                "classType": 2,
                 "isExotic": false
             }
         ],
         [
-            "RESISTENCIA",
+            "INTELECTO",
             "RECUPERACION",
             "DISCIPLINA"
         ]
     ],
     [
         [
-            83,
-            90,
-            84
+            60,
+            69,
+            69
         ],
         {
-            "id": "6917529734471420806",
+            "id": "6917529756259041037",
             "hash": 3787517196,
             "name": "Brazaletes Solares",
             "iconURL": "https://www.bungie.net/common/destiny2_content/icons/17c01ac034a7194e2de22d1ac487bb70.jpg",
-            "mobility": 2,
-            "resilience": 13,
-            "recovery": 18,
-            "discipline": 10,
-            "intellect": 7,
-            "strength": 16,
+            "mobility": 18,
+            "resilience": 8,
+            "recovery": 6,
+            "discipline": 19,
+            "intellect": 9,
+            "strength": 2,
             "armorType": "GAUNTLETS",
             "classType": 2,
             "isExotic": true
         },
         [
-            "RESISTENCIA",
-            "RESISTENCIA",
-            "RESISTENCIA",
-            "RESISTENCIA",
+            "DISCIPLINA",
+            "DISCIPLINA",
+            "DISCIPLINA",
+            "INTELECTO",
             "DISCIPLINA"
         ],
-        [],
+        [
+            "CONVERSOR DE ENERGIA",
+            "RESERVAS EXTRA",
+            "AMISTADES PODEROSAS",
+            "CARGA CON PRECISION"
+        ],
         [
             10,
-            10,
-            10
+            0,
+            -10
         ],
         [
             {
-                "id": "6917529549279540640",
-                "hash": 2345799798,
-                "name": "Grebas Sueño de la perdición",
-                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/2627c43a8d22136c3ad1f061bf92bc8e.jpg",
+                "id": "6917529705088755611",
+                "hash": 3465627817,
+                "name": "Túnica Lealtad Acolmillada",
+                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/5fe1451f524165401615226ab2bdc50c.jpg",
                 "mobility": 2,
-                "resilience": 8,
-                "recovery": 23,
-                "discipline": 21,
-                "intellect": 2,
-                "strength": 10,
-                "armorType": "LEGS",
-                "classType": 0,
+                "resilience": 2,
+                "recovery": 30,
+                "discipline": 9,
+                "intellect": 21,
+                "strength": 2,
+                "armorType": "CHEST",
+                "classType": 2,
                 "isExotic": false
             },
             {
-                "id": "6917529690901053094",
-                "hash": 3434445392,
-                "name": "Chaleco Sueño de la perdición",
-                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/f1c59c36f718a013b0d02601aeb672fe.jpg",
-                "mobility": 2,
+                "id": "6917529714800533653",
+                "hash": 693728753,
+                "name": "Botas Lealtad Acolmillada",
+                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/c1efda8fe1cdd34b01c289faa4a8078d.jpg",
+                "mobility": 6,
                 "resilience": 2,
-                "recovery": 29,
-                "discipline": 23,
-                "intellect": 6,
+                "recovery": 23,
+                "discipline": 21,
+                "intellect": 10,
                 "strength": 2,
-                "armorType": "CHEST",
-                "classType": 1,
+                "armorType": "LEGS",
+                "classType": 2,
                 "isExotic": false
             }
         ],
         [
-            "RESISTENCIA",
+            "INTELECTO",
             "RECUPERACION",
             "DISCIPLINA"
         ]
     ],
     [
         [
-            97,
-            106,
-            99
+            72,
+            92,
+            75
         ],
         {
-            "id": "6917529734471420806",
+            "id": "6917529756259041037",
             "hash": 3787517196,
             "name": "Brazaletes Solares",
             "iconURL": "https://www.bungie.net/common/destiny2_content/icons/17c01ac034a7194e2de22d1ac487bb70.jpg",
-            "mobility": 2,
-            "resilience": 13,
-            "recovery": 18,
-            "discipline": 10,
-            "intellect": 7,
-            "strength": 16,
+            "mobility": 18,
+            "resilience": 8,
+            "recovery": 6,
+            "discipline": 19,
+            "intellect": 9,
+            "strength": 2,
             "armorType": "GAUNTLETS",
             "classType": 2,
             "isExotic": true
         },
         [
-            "RESISTENCIA",
-            "RESISTENCIA",
-            "RESISTENCIA",
             "DISCIPLINA",
-            "RECUPERACION"
+            "INTELECTO",
+            "DISCIPLINA",
+            "INTELECTO",
+            "DISCIPLINA"
         ],
-        [],
+        [
+            "CONVERSOR DE ENERGIA",
+            "RESERVAS EXTRA",
+            "AMISTADES PODEROSAS",
+            "CARGA CON PRECISION"
+        ],
         [
             10,
-            10,
-            10
+            0,
+            -10
         ],
         [
             {
-                "id": "6917529549279540640",
-                "hash": 2345799798,
-                "name": "Grebas Sueño de la perdición",
-                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/2627c43a8d22136c3ad1f061bf92bc8e.jpg",
-                "mobility": 2,
-                "resilience": 8,
-                "recovery": 23,
-                "discipline": 21,
-                "intellect": 2,
-                "strength": 10,
-                "armorType": "LEGS",
-                "classType": 0,
-                "isExotic": false
-            },
-            {
-                "id": "6917529690901053094",
-                "hash": 3434445392,
-                "name": "Chaleco Sueño de la perdición",
-                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/f1c59c36f718a013b0d02601aeb672fe.jpg",
+                "id": "6917529705088755611",
+                "hash": 3465627817,
+                "name": "Túnica Lealtad Acolmillada",
+                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/5fe1451f524165401615226ab2bdc50c.jpg",
                 "mobility": 2,
                 "resilience": 2,
-                "recovery": 29,
-                "discipline": 23,
-                "intellect": 6,
+                "recovery": 30,
+                "discipline": 9,
+                "intellect": 21,
                 "strength": 2,
                 "armorType": "CHEST",
-                "classType": 1,
+                "classType": 2,
                 "isExotic": false
             },
             {
-                "id": "6917529784000436841",
-                "hash": 3015085684,
-                "name": "Yelmo Juramento del Legado",
-                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/b68b9045e25f4605680c7a541a7e8bba.jpg",
-                "mobility": 2,
-                "resilience": 24,
-                "recovery": 6,
-                "discipline": 15,
+                "id": "6917529714800533653",
+                "hash": 693728753,
+                "name": "Botas Lealtad Acolmillada",
+                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/c1efda8fe1cdd34b01c289faa4a8078d.jpg",
+                "mobility": 6,
+                "resilience": 2,
+                "recovery": 23,
+                "discipline": 21,
+                "intellect": 10,
+                "strength": 2,
+                "armorType": "LEGS",
+                "classType": 2,
+                "isExotic": false
+            },
+            {
+                "id": "6917529705094603607",
+                "hash": 849255710,
+                "name": "Capucha Lealtad Acolmillada",
+                "iconURL": "https://www.bungie.net/common/destiny2_content/icons/828b4bf6d01503cf4f9927113fabce90.jpg",
+                "mobility": 8,
+                "resilience": 2,
+                "recovery": 23,
+                "discipline": 16,
                 "intellect": 2,
                 "strength": 14,
                 "armorType": "HELMET",
-                "classType": 0,
+                "classType": 2,
                 "isExotic": false
             }
         ],
         [
-            "RESISTENCIA",
+            "INTELECTO",
             "RECUPERACION",
             "DISCIPLINA"
         ]
     ],
     [
         [
-            53,
-            48,
-            50
+            29,
+            26,
+            29
         ],
         {
-            "id": "6917529734471420806",
+            "id": "6917529756259041037",
             "hash": 3787517196,
             "name": "Brazaletes Solares",
             "iconURL": "https://www.bungie.net/common/destiny2_content/icons/17c01ac034a7194e2de22d1ac487bb70.jpg",
-            "mobility": 2,
-            "resilience": 13,
-            "recovery": 18,
-            "discipline": 10,
-            "intellect": 7,
-            "strength": 16,
+            "mobility": 18,
+            "resilience": 8,
+            "recovery": 6,
+            "discipline": 19,
+            "intellect": 9,
+            "strength": 2,
             "armorType": "GAUNTLETS",
             "classType": 2,
             "isExotic": true
         },
         [
             "DISCIPLINA",
-            "RESISTENCIA",
-            "RECUPERACION",
             "DISCIPLINA",
-            "RESISTENCIA"
+            "RECUPERACION",
+            "INTELECTO",
+            "DISCIPLINA"
+        ],
+        [
+            "CONVERSOR DE ENERGIA",
+            "RESERVAS EXTRA",
+            "AMISTADES PODEROSAS",
+            "CARGA CON PRECISION"
+        ],
+        [
+            10,
+            0,
+            -10
         ],
         [],
         [
-            10,
-            10,
-            10
-        ],
-        [],
-        [
-            "RESISTENCIA",
+            "INTELECTO",
             "RECUPERACION",
             "DISCIPLINA"
         ]
